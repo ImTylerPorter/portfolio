087a0ecb0bacb725318bd0c2b460668ef1d584a2
Tyler Porter <hello@cleverladder.com>
2024-12-11 19:47:19 -0800

    Add more projects

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -7,7 +7,7 @@
 		{
 			title: 'McClinton Auto',
 			description:
-				'A full-featured cms solution built with Svelte/SvelteKit, Drizzle and Supabase.',
+				'A full-featured CMS solution built with Svelte/SvelteKit, Drizzle, and Supabase.',
 			tech: ['SvelteKit', 'TypeScript', 'Supabase', 'Drizzle'],
 			github: 'https://github.com/ImTylerPorter/mcclinton-auto',
 			live: 'https://mcclintonauto.com'
@@ -15,7 +15,7 @@
 		{
 			title: 'Demo X/Y Plotting App',
 			description:
-				'App showing biodegradation data with a filter and simulation predicting degradation by days.',
+				'App showcasing biodegradation data with filtering capabilities and simulations predicting degradation over days.',
 			tech: ['SvelteKit', 'TypeScript', 'Supabase', 'Drizzle'],
 			github: 'https://github.com',
 			live: 'https://plotting-app.vercel.app/'
@@ -23,9 +23,55 @@
 		{
 			title: 'Federal CMS Solution',
 			description:
-				'Custom CMS built with PHP and WordPress. Specifically built in new features as client needed.',
+				'Custom CMS built with PHP and WordPress, incorporating new features as per client requirements.',
 			tech: ['PHP', 'WordPress', 'Gutenberg', 'SASS', 'JavaScript', 'Webpack'],
 			live: 'https://state.gov'
+		},
+		{
+			title: 'FED Design/Builders',
+			description:
+				"Developed a bespoke content management system utilizing PHP and WordPress, delivering a comprehensive end-to-end solution tailored to FED Design/Builders' specific needs.",
+			tech: ['PHP', 'WordPress', 'Gutenberg', 'SASS', 'JavaScript', 'Webpack'],
+			live: 'https://fedcorp.com/',
+			note: 'Note: This project has been handed off to the client. Further changes may have been made post-delivery.'
+		},
+		{
+			title: 'QWIK Raise',
+			description:
+				"Engineered a customized CMS with PHP and WordPress, providing a full-scale build that supports QWIK Raise's unique functionality and user experience requirements.",
+			tech: ['PHP', 'WordPress', 'Gutenberg', 'SASS', 'JavaScript', 'Webpack'],
+			live: 'https://qwikraise.com/',
+			note: 'Note: This project has been handed off to the client. Further changes may have been made post-delivery.'
+		},
+		{
+			title: 'Skywalker Property Partners',
+			description:
+				"Created a specialized WordPress and PHP-based content management system, managing the entire development lifecycle to enhance Skywalker Property Partners' online presence.",
+			tech: ['PHP', 'WordPress', 'Gutenberg', 'SASS', 'JavaScript', 'Webpack'],
+			live: 'https://www.skywalkerproperty.com/',
+			note: 'Note: This project has been handed off to the client. Further changes may have been made post-delivery.'
+		},
+		{
+			title: 'SearchWide Global',
+			description:
+				"Implemented a personalized CMS platform using PHP and WordPress, encompassing the complete build process to meet SearchWide Global's dynamic content management needs.",
+			tech: ['PHP', 'WordPress', 'Gutenberg', 'SASS', 'JavaScript', 'Webpack'],
+			live: 'https://searchwideglobal.com/',
+			note: 'Note: This project has been handed off to the client. Further changes may have been made post-delivery.'
+		},
+		{
+			title: 'View Israel',
+			description:
+				"Designed and built a tailored content management system with PHP and WordPress, ensuring a full-scale development that aligns with View Israel's vision and operational goals.",
+			tech: ['PHP', 'WordPress', 'Gutenberg', 'SASS', 'JavaScript', 'Webpack'],
+			live: 'https://view-israel.com/',
+			note: 'Note: This project has been handed off to the client. Further changes may have been made post-delivery.'
+		},
+		{
+			title: 'ScreenVue',
+			description: 'Custom CMS built with Next.js and Contentful.',
+			tech: ['Next.js', 'JavaScript', 'Contentful', 'Webpack'],
+			live: 'https://screenvue.com/'
 		}
 	];
 </script>
@@ -42,24 +88,27 @@
 						<Card.Title>{project.title}</Card.Title>
 					</Card.Header>
 					<Card.Content class="flex-grow">
-						<p class="text-muted-foreground mb-4">{project.description}</p>
+						<p class="mb-4 text-muted-foreground">{project.description}</p>
+						{#if project.note}
+							<p class="mb-4 text-sm italic text-gray-500">{project.note}</p>
+						{/if}
 						<div class="flex flex-wrap gap-2">
 							{#each project.tech as tech}
-								<span class="bg-primary/10 rounded-md px-2 py-1 text-sm">{tech}</span>
+								<span class="rounded-md bg-primary/10 px-2 py-1 text-sm">{tech}</span>
 							{/each}
 						</div>
 					</Card.Content>
 					<Card.Footer class="mt-auto flex justify-between pt-6">
 						{#if project?.github}
 							<Button variant="ghost" size="sm" asChild>
-								<a href={project.github} target="_blank">
+								<a href={project.github} target="_blank" rel="noopener noreferrer">
 									<Github class="mr-2 h-4 w-4" />
 									Code
 								</a>
 							</Button>
 						{/if}
 						<Button variant="ghost" size="sm" asChild class="ml-auto">
-							<a href={project.live} target="_blank">
+							<a href={project.live} target="_blank" rel="noopener noreferrer">
 								<ExternalLink class="mr-2 h-4 w-4" />
 								Live
 							</a>
